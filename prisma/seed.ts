9dd2fca6b681e26a577126815651f4b6b02fe669
kentanishida <kentanishida1118@gmail.com>
2023-10-01 15:14:53 +0900

    create db v1

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,25 +1,25 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
+// import { PrismaClient } from '@prisma/client';
+// const prisma = new PrismaClient();
 
-async function main() {
-  await prisma.user.upsert({
-    where: {
-      email: 'example@example.com',
-    },
-    create: {
-      email: 'example@example.com',
-      name: 'example',
-    },
-    update: {},
-  });
-}
+// async function main() {
+//   await prisma.user.upsert({
+//     where: {
+//       email: 'example@example.com',
+//     },
+//     create: {
+//       email: 'example@example.com',
+//       name: 'example',
+//     },
+//     update: {},
+//   });
+// }
 
-main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
+// main()
+//   .then(async () => {
+//     await prisma.$disconnect();
+//   })
+//   .catch(async (e) => {
+//     console.error(e);
+//     await prisma.$disconnect();
+//     process.exit(1);
+//   });
