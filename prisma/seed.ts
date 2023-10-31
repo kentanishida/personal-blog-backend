04c4f4e96c4469c55a77fca9bd415a6c28775f02
kentanishida <kentanishida1118@gmail.com>
2023-10-31 23:05:21 +0900

    seed 設定

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,25 +1,49 @@
-// import { PrismaClient } from '@prisma/client';
-// const prisma = new PrismaClient();
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
 
-// async function main() {
-//   await prisma.user.upsert({
-//     where: {
-//       email: 'example@example.com',
-//     },
-//     create: {
-//       email: 'example@example.com',
-//       name: 'example',
-//     },
-//     update: {},
-//   });
-// }
+async function main() {
+  await prisma.article.upsert({
+    where: {
+      id: '00000000-0000-0000-0000-000000000001',
+    },
+    create: {
+      id: '00000000-0000-0000-0000-000000000001',
+      status: 'PUBLIC',
+      title: {
+        connectOrCreate: {
+          where: {
+            id: '00000000-0000-0000-0000-000000000001',
+          },
+          create: {
+            id: '00000000-0000-0000-0000-000000000001',
+            name: 'テストタイトル',
+            text: 'テストタイトルです',
+          },
+        },
+      },
+      summary: {
+        connectOrCreate: {
+          where: {
+            id: '00000000-0000-0000-0000-000000000001',
+          },
+          create: {
+            id: '00000000-0000-0000-0000-000000000001',
+            name: 'テストサマリー',
+            text: 'この記事はこのことについて書かれておりますこの記事はこのことについて書かれておりますこの記事はこのことについて書かれておりますこの記事はこのことについて書かれておりますこの記事はこのことについて書かれておりますこの記事はこのことについて書かれておりますこの記事はこのことについて書かれておりますこの記事はこのことについて書かれておりますこの記事はこのことについて書かれておりますこの記事はこのことについて書かれております',
+          },
+        },
+      },
+    },
+    update: {},
+  });
+}
 
-// main()
-//   .then(async () => {
-//     await prisma.$disconnect();
-//   })
-//   .catch(async (e) => {
-//     console.error(e);
-//     await prisma.$disconnect();
-//     process.exit(1);
-//   });
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
