8f75c1e8eccd3f1d56d45bb2b6c071a5e58b557b
kentanishida <kentanishida1118@gmail.com>
2023-11-18 19:19:55 +0900

    pretter 設定

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,26 +22,26 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     //custom rules
-    "no-console": "warn",  // コンソールの使用を警告
-    "no-cond-assign": "error",  // 条件文内の代入を禁止
-    "no-constant-condition": "error",  // 常に真または偽となる条件を禁止
-    "no-duplicate-case": "error",  // switch文内の重複caseラベルを禁止
-    "no-empty": "error",  // 空ブロックを禁止
-    "no-ex-assign": "error",  // catch節内での例外変数の代入を禁止
-    "no-extra-parens": "error",  // 不必要な括弧を禁止
-    "no-unreachable": "error",  // 到達不可能なコードを禁止
-    "consistent-return": "error",  // 同一関数内のreturn文で一貫性を保持
-    "no-eval": "error",  // eval()の使用を禁止
-    "no-fallthrough": "error",  // switch文でのcase落ちを禁止
-    "no-implied-eval": "error",  // 暗黙的なeval()を禁止
-    "no-return-assign": "error",  // return文内での代入を禁止
-    "no-unused-expressions": "error",  // 使用されない式を禁止
-    "strict": ["error", "global"],  // 厳格モードを必須に
-    "camelcase": "error",  // キャメルケースの強制
-    "comma-spacing": "error",  // カンマの後の空白を強制
-    "no-dupe-class-members": "error",  // クラスメンバーの重複を禁止
-    "no-duplicate-imports": "error",  // 重複するimport文を禁止
-    "no-var": "error",  // varの使用を禁止
-    "prefer-const": "error",  // 再代入されない変数はconstを使用
+    'no-console': 'warn', // コンソールの使用を警告
+    'no-cond-assign': 'error', // 条件文内の代入を禁止
+    'no-constant-condition': 'error', // 常に真または偽となる条件を禁止
+    'no-duplicate-case': 'error', // switch文内の重複caseラベルを禁止
+    'no-empty': 'error', // 空ブロックを禁止
+    'no-ex-assign': 'error', // catch節内での例外変数の代入を禁止
+    'no-extra-parens': 'error', // 不必要な括弧を禁止
+    'no-unreachable': 'error', // 到達不可能なコードを禁止
+    'consistent-return': 'error', // 同一関数内のreturn文で一貫性を保持
+    'no-eval': 'error', // eval()の使用を禁止
+    'no-fallthrough': 'error', // switch文でのcase落ちを禁止
+    'no-implied-eval': 'error', // 暗黙的なeval()を禁止
+    'no-return-assign': 'error', // return文内での代入を禁止
+    'no-unused-expressions': 'error', // 使用されない式を禁止
+    strict: ['error', 'global'], // 厳格モードを必須に
+    camelcase: 'error', // キャメルケースの強制
+    'comma-spacing': 'error', // カンマの後の空白を強制
+    'no-dupe-class-members': 'error', // クラスメンバーの重複を禁止
+    'no-duplicate-imports': 'error', // 重複するimport文を禁止
+    'no-var': 'error', // varの使用を禁止
+    'prefer-const': 'error', // 再代入されない変数はconstを使用
   },
 };
